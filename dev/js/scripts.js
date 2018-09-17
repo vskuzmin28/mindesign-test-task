27fe1157abf4903cb51cb0a43304173355e1ba3b
vskuzmin28 <vskuzmin28@gmail.com>
2018-09-17 11:48:17 +0300

    added animation header

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,16 +1,32 @@
-// call popup
+// call button promo portal
 
-$('.popup-call').hide();
-$('.link-popup-call').click(function(e){
-  e.preventDefault();
-  $('.popup-call').bPopup({
-    //closeClass:'popup__icon-close',
-      amsl: 0,
-      positionStyle: 'fixed',
-    })
+//$('.popupCallPromoPortal').hide();
+//$('.link-popup-call').click(function(e){
+//  e.preventDefault();
+//  $('.popup-call').bPopup({
+//    //closeClass:'popup__icon-close',
+//      amsl: 0,
+//      positionStyle: 'fixed',
+//    })
+//})
+
+// header
+
+$(window).scroll(function() {
+  if($(window).scrollTop() >= 120) {
+
+    $('.header').addClass('header_fixed');
+    $('.header__logo').addClass('header__logo-to-scroll');
+
+  } else {
+
+    $('.header').removeClass('header_fixed');
+    $('.header__logo').removeClass('header__logo-to-scroll');
+
+  }   
 })
 
-// portfolio
+// main slider
 
 $('.main-slider__container').responsiveSlides({
   auto: true,
